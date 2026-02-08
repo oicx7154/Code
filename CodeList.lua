19a34f2153039164322810ff46839af0715629ca
oicx7154 <oicx7154@cpaar.com>
2026-02-08 11:33:37 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -3,7 +3,8 @@ return {
     "200Kmembers",
     "INFINITE",
     "WOW100KLIKES",
-    "110KLIKES"
+    "110KLIKES",
+    "ProjectionSorcery"
     };
     cn= "更新了两个兑换码",
     en = "Update two codes"
