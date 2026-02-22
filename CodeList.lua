e210db0b320a7f158866fbbe18346e6ffa8cef00
oicx7154 <oicx7154@cpaar.com>
2026-02-22 11:21:31 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -2,6 +2,7 @@ return {
     Code = {
     "130KLIKES",
     "SPLITSOUL",
+    "walls",
     };
-    Update= "更新了两个兑换码,移除了2个code\nUpdate two codes,Removed two codes\n2026/2/22",
+    Update= "更新了三个兑换码,移除了2个code\nUpdate three codes,Removed two codes\n2026/2/22",
 }
