fe363c986ece853e142158b281f1107b8da0d59e
oicx7154 <oicx7154@cpaar.com>
2026-03-22 13:13:11 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -1,8 +1,7 @@
 return {
     Code = {
-    "HAKARI",
     "QOLL",
     "140KLIKES",
     };
-    Update= "更新了1个兑换码,移除了2个code\nUpdate one codes,Removed two codes\n2026/3/22",
+    Update= "更新了1个兑换码,移除了3个code\nUpdate one codes,Removed three codes\n2026/3/22",
 }
