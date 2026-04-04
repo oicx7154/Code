b193d9dd8310a44d3742adfb7b2724b1696efae7
oicx7154 <oicx7154@cpaar.com>
2026-04-05 00:06:37 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -1,8 +1,7 @@
 return {
     Code = {
-    "140KLIKES",
-    "JUDGEMAN",
-    "mapIMPROVED"
+    "150KLIKES",
+    "RYUU",
     };
-    Update= "更新了1个兑换码,移除了1个code\nUpdate one codes,Removed one codes\n2026/4/3",
+    Update= "更新了2个兑换码,移除了3个code\nUpdate two codes,Removed three codes\n2026/4/5",
 }
