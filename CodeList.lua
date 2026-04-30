8c93ffcaef7808deb02520278c2cad92987e6f94
oicx7154 <oicx7154@cpaar.com>
2026-04-30 21:31:32 +0800

    Update codes and modify update message in CodeList

--- CodeList.lua
+++ CodeList.lua
@@ -2,6 +2,8 @@ return {
     Code = {
     "150KLIKES",
     "TRADING",
+    "YUTA",
+    "160KLIKES",
     };
-    Update= "更新了1个兑换码,移除了1个code\nUpdate one codes,Removed two codes\n2026/4/12",
+    Update= "更新了2个兑换码,移除了0个code\nUpdate two codes,Removed zero codes\n2026/4/30",
 }
