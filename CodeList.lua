5906beceee1ed839718607da059c82a43258a20e
oicx7154 <oicx7154@cpaar.com>
2026-05-10 02:13:53 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -3,5 +3,5 @@ return {
     "YUTAPARTTWO",
     "160KLIKES",
     };
-    Update= "更新了1个兑换码,移除了3个code\nUpdate one codes,Removed three codes\n2026/4/30",
+    Update= "更新了1个兑换码,移除了3个code\nUpdate one codes,Removed three codes\n2026/5/10",
 }
