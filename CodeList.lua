80761c6a929cf8b4ceb2ba895be6354fb74b5c1f
oicx7154 <oicx7154@cpaar.com>
2026-05-10 02:13:28 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -1,9 +1,7 @@
 return {
     Code = {
-    "150KLIKES",
-    "TRADING",
-    "BLACKFLASH",
+    "YUTAPARTTWO",
     "160KLIKES",
     };
-    Update= "更新了1个兑换码,移除了1个code\nUpdate onew codes,Removed one codes\n2026/4/30",
+    Update= "更新了1个兑换码,移除了3个code\nUpdate one codes,Removed three codes\n2026/4/30",
 }
