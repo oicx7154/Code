341cbe472c28dc3d6b7f0e92ddb109c1743f1f73
oicx7154 <oicx7154@cpaar.com>
2026-05-31 18:30:34 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -3,6 +3,7 @@ return {
     "HEAVENLYRESSSS ",
     "170KLIKES",
     "AURA",
+    "ZENIN",
     };
-    Update= "更新了2个兑换码,移除了1个code\nUpdate two codes,Removed one codes\n2026/5/24",
+    Update= "更新了1个兑换码,移除0个code\n可能有些无效了\n日期:2026/5/31",
 }
