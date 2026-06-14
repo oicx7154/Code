4fae12389d017c8af85041913f36f74effdbfa51
oicx7154 <oicx7154@cpaar.com>
2026-06-14 10:36:31 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -1,9 +1,8 @@
 return {
     Code = {
-    "HEAVENLYRESSSS ",
+    "250KMEMBERS",
     "170KLIKES",
-    "AURA",
-    "BLOODV2",
+    "BLOODDOMAIN",
     };
-    Update= "更新了1个兑换码,移除1个code\n可能有些无效了\n日期:2026/6/7",
+    Update= "更新了3个兑换码,移除4个code\n可能有些无效了\n日期:2026/6/14",
 }
