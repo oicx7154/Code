e38feeab0cab8690f924a50f6697c73d4dc4949d
oicx7154 <oicx7154@cpaar.com>
2026-08-07 16:03:44 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -1,7 +1,7 @@
 return {
     Code = {
-    "SUMMERBLADEfor",
+    "MAHORAGA",
     "190KLIKES",
     };
-    Update= "更新了1个兑换码\n移除1个code\n可能有些无效了\n日期:2026/7/26",
+    Update= "更新了1个兑换码\n移除1个code\n可能有些无效了\n日期:2026/8/7",
 }
