bc10ecdd9117d9bddc46d88957a803652490f573
oicx7154 <oicx7154@cpaar.com>
2026-08-24 05:19:03 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -1,7 +1,7 @@
 return {
     Code = {
     "200KLIKES",
-    "CTBUFFS",
+    "CALAMITY",
     };
-    Update= "更新了1个兑换码\n移除1个code\n可能有些无效了\n日期:2026/8/16",
+    Update= "更新了1个兑换码\n移除1个code\n可能有些无效了\n日期:2026/8/24",
 }
