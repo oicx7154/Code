8a270fe4a861258e13f898a8faad73969027f8b0
oicx7154 <oicx7154@cpaar.com>
2026-09-06 13:38:48 +0800

    Update CodeList.lua

--- CodeList.lua
+++ CodeList.lua
@@ -1,7 +1,7 @@
 return {
     Code = {
-    "200KLIKES",
-    "AWAKENED",
+    "210KLIKES",
+    "INFINITEVOID",
     };
-    Update= "更新了1个兑换码\n移除1个code\n可能有些无效了\n日期:2026/8/30",
+    Update= "更新了2个兑换码\n移除2个code\n可能有些无效了\n日期:2026/9/6",
 }
